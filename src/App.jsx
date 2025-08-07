d46f5ce8c785416ba113e14227c5469cb30a23f0
SunainaNelli <sunaina.nelli@gmail.com>
2025-08-07 15:43:29 +0530

    Routing related update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useContext} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import { PicklesProvider, PicklesContext } from './context/PicklesContext';
 
